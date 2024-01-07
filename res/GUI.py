0698944273731c878c73ce86fa16ded92e2ca8a9
Matthew Mumbere <matthewmumbere@gmail.com>
2024-01-07 22:03:38 +0300

    Search Panel Created
    Search Bar and The Welcome Header was set up

--- res/GUI.py
+++ res/GUI.py
@@ -100,6 +100,10 @@ class Search_Panel(QFrame):
         super().__init__()  
         self.init = init
         self.init.register("Search_Panel", self)
+        
+        self.search_bar = Search_Bar(INIT)
+        self.welcome_header = Welcome_Home_Label(INIT)
+
         self.setup()
 
     def setup(self) -> None:
@@ -129,3 +133,54 @@ class Search_Panel(QFrame):
         self.Right_Home_Spacer = QSpacerItem(40, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
 
         self.gridLayout_2.addItem(self.Right_Home_Spacer, 1, 2, 1, 1)
+
+        self.gridLayout_2.addWidget(self.search_bar, 2, 1, 1, 1)
+
+        self.gridLayout_2.addWidget(self.welcome_header, 1, 1, 1, 1)
+
+
+
+class Search_Bar(QLineEdit):
+    def __init__(self, init: Initializer) -> None:
+        super().__init__() 
+        self.init = init
+        self.init.register("Search_Bar", self)
+        self.setup()
+
+    def setup(self) -> None:
+        self.setObjectName(u"SearchBar")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy1)
+        self.setMinimumSize(QSize(400, 50))
+        self.setPlaceholderText("Search For Manga...")
+
+class Welcome_Home_Label(QLabel):
+    def __init__(self, init: Initializer) -> None:
+        super().__init__()  
+        self.init = init
+        self.init.register("Welcome_Home_Label", self)
+
+        self.setup()
+        self.signals()
+    def setup(self) -> None:
+        self.setObjectName(u"Welcome_Header")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy2)
+        self.setMinimumSize(QSize(0, 50))
+        font = QFont()
+        font.setFamilies([u"FreeSerif"])
+        font.setPointSize(30)
+        font.setBold(True)
+        font.setItalic(True)
+        self.setFont(font)
+        self.setAlignment(Qt.AlignCenter)
+        self.setText("Welcome To Manga Library")
+
+    def signals(self) -> None:
+        self.returnPressed()
